1d0f529a0b1a7f0a4c0d5c838bf8f0423abafbbf
VirgilioMacero <virgiliomacero@gmail.com>
2021-04-30 10:59:07 -0400

    Modificacion de consultas
    Se modificaron las consultas y el codigo

--- SeguimientoAlumnos/SeguimientoAlumnos/MainWindow.xaml.cs
+++ SeguimientoAlumnos/SeguimientoAlumnos/MainWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace SeguimientoAlumnos
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -35,66 +36,35 @@ namespace SeguimientoAlumnos
         private void BtnIniciarProfesor_Click(object sender, RoutedEventArgs e)
         {
 
-            string conexion = "datasource=127.0.0.1;port=3306;username=root;password=;database=sistema_seguimiento";
-
-            string query = "SELECT * FROM profesor";
-
-
-            MySqlConnection ConexionDataBase = new  MySqlConnection(conexion);
-
-            MySqlCommand ConsultaProfesor = new MySqlCommand(query,ConexionDataBase);
-            MySqlDataReader leer;
-            try
-            {
+            MySqlConnection ConexionDataBase = new  MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=sistema_seguimiento");
             ConexionDataBase.Open();
+            MySqlCommand Consulta = new MySqlCommand();
+            Consulta.Connection = ConexionDataBase;
+            Consulta.CommandText = ("select * FROM profesor where RUT ='"+this.TxtUsuarioProfesor.Text +"' and Contrasenia ='" + this.TxtContraseniaProfesor.Text +" ' ");
 
-                leer = ConsultaProfesor.ExecuteReader();
-
-                if (leer.HasRows)
-                {
-
-                    while (leer.Read())
-                    {
-
-                        string Usuario =  leer.GetString(3);
-
-                        string Clave = leer.GetString(4);
-
-                        if (Usuario == TxtUsuarioProfesor.Text && Clave == TxtContraseniaProfesor.Text)
-                        {
+            MySqlDataReader Leer = Consulta.ExecuteReader();
 
 
-
-                            VistaProfesor VistaProfe = new VistaProfesor();
-
-                            VistaProfe.LblNombreProfesor.Content = leer.GetString(2);
-
-                            VistaProfe.Show();
-
-                            break;
-
-                        }
-                        else
-                        {
-                            MessageBox.Show("El usuario o la contraseña son incorrectos");
-                        }
-
-                    }
-
-
-                }
-                else
-                {
-
-                    MessageBox.Show("No se encontraron datos");
-
-                }
-
-            }
-            catch
+            if (Leer.Read())
             {
 
+                VistaProfesor VistaProfe = new VistaProfesor();
+
+                VistaProfe.LblNombreProfesor.Content = Leer.GetValue(2);
+
+                VistaProfe.Show();
+
+
             }
+            else
+            {
+
+                MessageBox.Show("Algun dato esta mal introducido");
+
+            }
+
+            ConexionDataBase.Close();
+
 
 
         }
@@ -102,76 +72,112 @@ namespace SeguimientoAlumnos
         private void BtnIniciarAlumno_Click(object sender, RoutedEventArgs e)
         {
 
-            string conexion = "datasource=127.0.0.1;port=3306;username=root;password=;database=sistema_seguimiento";
 
-            string query = "SELECT * FROM alumno";
+            MySqlConnection ConexionDataBase = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=sistema_seguimiento");
+            ConexionDataBase.Open();
+            MySqlCommand Consulta = new MySqlCommand();
+            Consulta.Connection = ConexionDataBase;
+            Consulta.CommandText = ("select * FROM alumno where RUT ='" + this.TxtUsuarioAlumno.Text + "' and Contrasenia ='" + this.TxtContraseniaAlumno.Text + " ' ");
 
+            MySqlDataReader Leer = Consulta.ExecuteReader();
 
-            MySqlConnection ConexionDataBase = new MySqlConnection(conexion);
-
-            MySqlCommand ConsultaProfesor = new MySqlCommand(query, ConexionDataBase);
-            MySqlDataReader leer;
-
-
-            try
-            {
-                ConexionDataBase.Open();
-
-                leer = ConsultaProfesor.ExecuteReader();
-
-                if (leer.HasRows)
-                {
-
-                    while (leer.Read())
-                    {
-
-                        string Usuario = leer.GetString(6);
-
-                        string Clave = leer.GetString(7);
-
-                        if (Usuario == TxtUsuarioAlumno.Text && Clave == TxtContraseniaAlumno.Text)
-                        {
-
-
-                            var Alumnito2 = new Alumno();
-
-                            Alumnito2.Nombre = leer.GetString(5);
-                            Alumnito2.Correo = leer.GetString(4);
-                            Alumnito2.RUT = leer.GetString(6);
-                            Alumnito2.Clave = leer.GetString(7);
-
-
-
-                            VistaAlumno VistaAlumno1 = new VistaAlumno(Alumnito2);
-
-                            
-
-                            
-
-                            break;
-
-                        }
-                        else
-                        {
-                            MessageBox.Show("El usuario o la contraseña son incorrectos");
-                        }
-
-                    }
-
-
-                }
-                else
-                {
-
-                    MessageBox.Show("No se encontraron datos");
-
-                }
-
-            }
-            catch
+            if (Leer.Read())
             {
 
+                var Alumnito2 = new Alumno();
+
+                Alumnito2.Nombre = Leer.GetString(5);
+                Alumnito2.Correo = Leer.GetString(4);
+                Alumnito2.RUT = Leer.GetString(6);
+                Alumnito2.Clave = Leer.GetString(7);
+
+
+
+                VistaAlumno VistaAlumno1 = new VistaAlumno(Alumnito2);
+
+                VistaAlumno1.Show();
+
+
             }
+            else
+            {
+
+                MessageBox.Show("Hay algun dato mal ingresado");
+
+            }
+
+
+
+            //string conexion = "datasource=127.0.0.1;port=3306;username=root;password=;database=sistema_seguimiento";
+
+            //string query = "SELECT * FROM alumno";
+
+
+            //MySqlConnection ConexionDataBase = new MySqlConnection(conexion);
+
+            //MySqlCommand ConsultaProfesor = new MySqlCommand(query, ConexionDataBase);
+            //MySqlDataReader leer;
+
+
+            //try
+            //{
+            //    ConexionDataBase.Open();
+
+            //    leer = ConsultaProfesor.ExecuteReader();
+
+            //    if (leer.HasRows)
+            //    {
+
+            //        while (leer.Read())
+            //        {
+
+            //            string Usuario = leer.GetString(6);
+
+            //            string Clave = leer.GetString(7);
+
+            //            if (Usuario == TxtUsuarioAlumno.Text && Clave == TxtContraseniaAlumno.Text)
+            //            {
+
+
+            //                var Alumnito2 = new Alumno();
+
+            //                Alumnito2.Nombre = leer.GetString(5);
+            //                Alumnito2.Correo = leer.GetString(4);
+            //                Alumnito2.RUT = leer.GetString(6);
+            //                Alumnito2.Clave = leer.GetString(7);
+
+
+
+            //                VistaAlumno VistaAlumno1 = new VistaAlumno(Alumnito2);
+
+            //                VistaAlumno1.Show();
+
+
+
+            //                break;
+
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("El usuario o la contraseña son incorrectos");
+            //            }
+
+            //        }
+
+
+            //    }
+            //    else
+            //    {
+
+            //        MessageBox.Show("No se encontraron datos");
+
+            //    }
+
+            //}
+            //catch
+            //{
+
+            //}
 
 
 
