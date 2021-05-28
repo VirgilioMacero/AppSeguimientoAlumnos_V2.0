d92cabe5a31d82c052e274aea67374a7ba43a53e
gimeno1313 <82778566+gimeno1313@users.noreply.github.com>
2021-05-27 22:14:13 -0400

    se agrego los atributos a seguimiento

--- SeguimientoAlumnos/SeguimientoAlumnos/Seguimiento.cs
+++ SeguimientoAlumnos/SeguimientoAlumnos/Seguimiento.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace SeguimientoAlumnos
 {
-    class Seguimiento
+    class Seguimiento  //agregue la clase seguimiento
     {
+        public string Causa { get; set; }   //agregue string causa
+
+        public int ID { get; set; } //agregue int ID
+
+        public string Mensaje { get; set; } //agregue string Mensaje
+
+
+
     }
 }
